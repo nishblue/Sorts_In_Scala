5b570e56a037d8b9f9293574e4bfd40de713a693
Nishit Jain <nishblue@gmail.com>
2016-08-13 15:48:47 -0500

    Bubble Sort

---
+++ src/BubbleSort.scala
@@ -0,0 +1,45 @@
+/**
+  * Created by stephaniejain on 8/13/16.
+  */
+object BubbleSort {
+
+  def swapElem(iA: List[Int],currIndex : Int): List[Int] ={
+
+    if(iA.length == currIndex +1){
+      iA
+    }
+    else if(iA(currIndex) < iA(currIndex+1)) {
+      val newA : List[Int] = iA.updated(currIndex, iA(currIndex + 1))
+      swapElem(newA.updated(currIndex+1,iA(currIndex)),currIndex+1)
+    }
+    else{
+      swapElem(iA, currIndex+1)
+    }
+
+  }
+
+  def bubbleSort(iA: List[Int],currIndex : Int): List[Int] = {
+    if(iA.length == currIndex +1) {
+      iA
+    }else{
+      bubbleSort(swapElem(iA,0),currIndex+1)
+    }
+
+  }
+
+
+  def main(args: Array[String]) {
+    val inputList = List(787777,3, 1,8878, 6, 8, 10,0,99999, 9, 5, 88, 33,989, 22, 11)
+    val inputRange = 0 to inputList.length-1
+
+    //BubbleSort.swapElem(inputList,0).foreach(println)
+
+    BubbleSort.bubbleSort(inputList,0).foreach(println)
+
+
+  }
+
+
+}
+
+
